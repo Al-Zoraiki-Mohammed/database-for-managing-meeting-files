632192ecd263ad181887b3032467f483b0b34559
Al-Zoraiki-Mohammed <mama.zoraiki2017@gmail.com>
2023-11-18 15:25:04 +0100

    'update'

---
+++ Creat_database_v2.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE paperless_university_v2;
+USE paperless_university_v2;
+
+CREATE TABLE Users  (
+    UserID INT PRIMARY KEY,
+    UserName VARCHAR(50),
+    Email VARCHAR(100),
+    Password VARCHAR(255), -- Use appropriate length for password hashes
+    UserType ENUM('Admin', 'Regular User')
+);
+
+CREATE TABLE Meetings (
+    MeetingID INT PRIMARY KEY,
+    Title VARCHAR(100),
+    Description TEXT,
+    Date DATE,
+    Time TIME,
+    Location VARCHAR(255),
+    OrganizerID INT,
+    Status ENUM('Scheduled', 'In Progress', 'Completed', 'Canceled'),
+    FOREIGN KEY (OrganizerID) REFERENCES Users(UserID)
+);
+
+CREATE TABLE Files (
+    FileID INT PRIMARY KEY,
+    FileName VARCHAR(255),
+    FilePath VARCHAR(255),
+    MeetingID INT,
+    UploadedByUserID INT,
+    UploadDateTime TIMESTAMP,
+    FOREIGN KEY (MeetingID) REFERENCES Meetings(MeetingID),
+    FOREIGN KEY (UploadedByUserID) REFERENCES Users(UserID)
+);
+
+CREATE TABLE Interactions (
+    InteractionID INT PRIMARY KEY,
+    -- MeetingID INT,  -- when the interaction with meeting
+    UserID INT,
+    FileID INt,        
+    InteractionType VARCHAR(50),
+    InteractionContent TEXT,
+    InteractionDateTime TIMESTAMP,
+    -- FOREIGN KEY (MeetingID) REFERENCES Meetings(MeetingID),  -- cancelled
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+     FOREIGN KEY (FileID) REFERENCES Files(FileID)
+);
+
+/*
+CREATE TABLE Votes (
+    VoteID INT PRIMARY KEY,
+    MeetingID INT,
+    UserID INT,
+    VoteType VARCHAR(50),
+    VoteDateTime TIMESTAMP,
+    FOREIGN KEY (MeetingID) REFERENCES Meetings(MeetingID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+); 
+Cancelled because the votes for files not about attending meetings or not as I thought
+ */
+
+
+
+
---
+++ insert_data_samples_v2.sql
@@ -0,0 +1,32 @@
+USE paperless_university_v2;
+
+INSERT INTO Users (UserID, UserName, Email, Password, UserType)
+	VALUES (1, 'Mohammed Al-Zoraiki', 'mama@gamil.com', 'hashed_password', 'Admin'),
+	(2, 'Hamza', 'Hamza@gamil.com', 'hashed_password', 'Admin'),
+	(3, 'Eliza', 'Eliza@gamil.com', 'hashed_password', 'Admin'),
+	(4, 'Labar', 'Labar@gamil.com', 'hashed_password', 'Admin'),
+	(5, 'Ali', 'Ali@gamil.com', 'hashed_password', 'Regular User'),
+	(6, 'Ahmed', 'Ahmed@gamil.com', 'hashed_password', 'Regular User');
+        
+        
+INSERT INTO Meetings (MeetingID, Title, Description, Date, Time, Location, OrganizerID, Status)
+	VALUES (1, 'Project Kickoff', 'Meeting to discuss project goals', '2023-11-7', '20:00:00', 'Conference Room A', 1, 'Scheduled');
+    
+    
+    
+    
+INSERT INTO Files (FileID, FileName, FilePath, MeetingID, UploadedByUserID, UploadDateTime)
+VALUES (1, 'example_file.txt', 'somewhere/uploads/example_file.txt', 1, 1, '2023-11-10 15:00:00');
+
+INSERT INTO Interactions (InteractionID, FileID, UserID, InteractionType, InteractionContent, InteractionDateTime)
+	VALUES (1, 1, 3, 'Comment', 'Great discussion!', '2023-11-10 14:30:00'),
+           (2, 1, 4, 'Vote', 'Agree', '2023-11-10 14:30:00');
+
+/* 
+INSERT INTO Votes (VoteID, MeetingID, UserID, VoteType, VoteDateTime)
+	VALUES (1, 1, 1, 'Agree', '2023-11-10 14:45:00');
+*/
+    
+ 
+
+
